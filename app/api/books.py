f7c644e495f1d67cde977ed667a57e4ee32dcba5
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-27 00:31:16 +0200

    added exceptions in router

--- app/api/books.py
+++ app/api/books.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Query, status, HTTPException
 
 from app.schemas.book import BookStatus, BookResponse, BookCreate, BooksPage
 from app.dependencies.dependencies import BookServiceDep
+from app.exceptions import BookNotFoundError, BookCreateError
 
 router = APIRouter(prefix="/books", tags=["Books"])
 
@@ -23,23 +24,23 @@ async def get_books(
 
 @router.get("/{book_id}", response_model=BookResponse)
 async def get_book(book_id: UUID, service: BookServiceDep):
-    book = await service.get_book(book_id)
-    if not book:
+    try:
+        return await service.get_book(book_id)
+    except BookNotFoundError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
-    return book
 
 
 @router.post("/", response_model=BookResponse, status_code=status.HTTP_201_CREATED)
 async def create_book(book: BookCreate, service: BookServiceDep):
     try:
         return await service.create(book)
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create book")
+    except BookCreateError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: UUID, service: BookServiceDep):
-    book = await service.get_book(book_id)
-    if not book:
+    try:
+        await service.delete_book(book_id)
+    except BookNotFoundError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
-    await service.delete_book(book_id)
